e232b22ef49b61ffe63215ff2e88be77434d6942
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-09-05 19:24:54 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/piquette/finance-go"
-	"github.com/piquette/finance-go/quote"
+	"github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/datetime"
+	"github.com/piquette/finance-go/equity"
+	"github.com/shopspring/decimal"
 )
 
-func main() {
-	var stockList [10]finance.Quote
+type Stock struct {
+	quote             finance.Equity
+	growth5yrs        float64
+	growth5yrsPercent float64
+	potentialEarning  float64
+	potentialLoss     float64
+}
 
-	q, err := quote.Get("ABR")
+func newStock(symbol string) *Stock {
+	q, err := equity.Get(symbol)
 	if err != nil {
-		panic(err)
+		return (nil)
 	}
 
-	println(q.Symbol)
+	params := &chart.Params{
+		Symbol:   q.Quote.Symbol,
+		Interval: datetime.OneMonth,
+		Start:    &datetime.Datetime{Month: 1, Day: 1, Year: 2000},
+		End:      &datetime.Datetime{Month: int(time.Now().Month()), Day: time.Now().Day(), Year: time.Now().Year()},
+	}
+	iter := chart.Get(params)
+
+	var priceArray []decimal.Decimal
+	//var growth5yrs float64 = 0
+	//var growth5yrsPercent float64 = 0
+	//var monthCounter int = 0
+
+	for iter.Next() {
+		priceArray = append(priceArray, iter.Bar().Close)
+	}
+
+	println(len(priceArray))
+
+	if err := iter.Err(); err != nil {
+		return nil
+	}
+
+	print("[" + q.Symbol + "] Price: " + fmt.Sprintf("%f", q.RegularMarketPrice))
+
+	return &Stock{*q, 0, 0, 0, 0}
+}
+
+func main() {
+	newStock("f")
 }
